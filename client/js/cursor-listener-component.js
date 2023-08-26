0f3ba62d94eb245ebef61228df820161efe268ef
leandrodevarona <leandrodevarona9908@gmail.com>
2023-08-26 11:56:27 -0400

    init project

--- client/js/cursor-listener-component.js
+++ client/js/cursor-listener-component.js
@@ -10,29 +10,11 @@ AFRAME.registerComponent('cursor-listener', {
     this.el.addEventListener('raycaster-intersection', evt => {
       const intersectedEl = evt.detail.intersections[0].object.el;
       const cursor = document.getElementById(this.data.disable_cursor);
-      console.log(
-        'La clase es..................',
-        intersectedEl.getAttribute('class')
-      );
       if (intersectedEl.getAttribute('class') === 'babiaxraycasterclass') {
-        console.log('Entra aqui....................... if');
         cursor.setAttribute('raycaster', 'objects: disabled_now');
       } else {
-        console.log('Entra aqui....................... else');
         cursor.setAttribute('raycaster', 'objects: [floor]');
       }
     });
-  },
-
-  update: function () {
-    // Do something when component's data is updated.
-  },
-
-  remove: function () {
-    // Do something the component or its entity is detached.
-  },
-
-  tick: function (time, timeDelta) {
-    // Do something on every scene tick or frame.
   },
 });
