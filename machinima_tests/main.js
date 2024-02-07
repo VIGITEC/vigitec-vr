ff20cf985b390ad33a2d05110db75d9a4f4885c9
leandrodevarona <135623705+leandrodevarona@users.noreply.github.com>
2024-02-07 15:55:41 -0500

    Quitando machinima tests

--- machinima_tests/main.js
+++
@@ -1,7 +0,0 @@
-window.debug = true;
-// include all dependencies via require - don't use script tags in scenes
-require('aframe');
-require('aframe-motion-capture-components');
-// require your package entry point:
-require('../index.js');
-require('./scenes/js/movement-component.js')
